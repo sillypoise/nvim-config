5623b8efcfd5cf59a3c93541e4d6198e5c50483d
joserosas-vanta <jose.rosas@vanta.com>
2023-11-01 19:14:23 -0500

    Add flexoki theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,16 @@
 -- Or with configuration
 return {
-  "projekt0n/github-nvim-theme",
+  -- "projekt0n/github-nvim-theme",
+  "kepano/flexoki-neovim",
   opts = {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
+    name = "flexoki",
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_dark",
+      colorscheme = "flexoki-dark",
     },
   },
 }
