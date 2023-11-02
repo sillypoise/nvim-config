81492d6ba9c3b21b13872c15636b94a26a2de346
joserosas-vanta <jose.rosas@vanta.com>
2023-11-01 19:03:11 -0500

    Add github theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+-- Or with configuration
+return {
+  "projekt0n/github-nvim-theme",
+  opts = {
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark",
+    },
+  },
+}
