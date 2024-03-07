50c9f9d3893691c55444e745777daf29b23f0835
sillypoise <somewhatjose@gmail.com>
2024-03-07 16:42:10 -0500

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,17 @@
 -- Or with configuration
 return {
-  -- "projekt0n/github-nvim-theme",
-  "kepano/flexoki-neovim",
-  opts = {
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    name = "flexoki",
-  },
+  "projekt0n/github-nvim-theme",
+  -- "kepano/flexoki-neovim",
+  -- opts = {
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   name = "flexoki",
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "flexoki-dark",
+      -- colorscheme = "flexoki-dark",
+      colorscheme = "github_dark_high_contrast",
     },
   },
 }
