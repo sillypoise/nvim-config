3efe33127263e95fc47a6bd4c406828477e3da7e
sillypoise <somewhatjose@gmail.com>
2024-11-26 18:59:07 -0500

    update colorscheme to be transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,13 @@
--- Or with configuration
 return {
   "projekt0n/github-nvim-theme",
-  -- "kepano/flexoki-neovim",
-  -- opts = {
-  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   name = "flexoki",
-  -- },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "flexoki-dark",
-      colorscheme = "github_dark_high_contrast",
-    },
-  },
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require("github-theme").setup({
+      options = {
+        transparent = true,
+      },
+    })
+    vim.cmd("colorscheme github_dark_high_contrast")
+  end,
 }
